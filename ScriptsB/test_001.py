e6232e6950fb160599109c0c13ddad2086f70c4c
zhangfuying1991 <2315183316@qq.com>
2019-01-26 22:20:32 +0800

    jianxiangmu

---
+++ ScriptsB/test_001.py
@@ -0,0 +1,9 @@
+class Test_001:
+
+    def test_001_01(self):
+        print("-----test_001-1")
+        assert True
+
+    def test_001_02(self):
+        print("-----test_001-2")
+        assert False
